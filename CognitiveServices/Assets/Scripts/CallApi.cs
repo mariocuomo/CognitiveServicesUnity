69c016c2ec4d270d6dc14fe93a5a9191f2ddcd23
Mario Cuomo <cuomomario@hotmail.com>
2019-02-03 21:32:19 +0100

    First Commit

---
+++ CognitiveServices/Assets/Scripts/CallApi.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.Networking;
+using System;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+
+public class CallApi : MonoBehaviour
+{
+
+    string visionKey = "visionKEY"; // replace with your Vision API Key
+    string resourceURL = "https://westeurope.api.cognitive.microsoft.com/vision/v2.0/analyze?details=Landmarks&language=en";
+
+    public string imageName { get; private set; }
+    string responseData;
+
+    // Use this for initialization
+    void Start()
+    {
+        imageName = Path.Combine(Application.streamingAssetsPath, "foto.jpeg");
+    }
+
+    IEnumerator GetInfo()
+    {
+        imageName = Path.Combine(Application.streamingAssetsPath, "foto.jpeg"); 
+        byte[] bytesImage = UnityEngine.Windows.File.ReadAllBytes(imageName);
+
+        var headers = new Dictionary<string, string>() {
+            { "Ocp-Apim-Subscription-Key", visionKey },
+            { "Content-Type", "application/octet-stream" }
+        };
+
+        WWW www = new WWW(resourceURL, bytesImage, headers);
+
+        yield return www;
+        responseData = www.text;
+        Debug.Log(responseData);
+        JSONApi answer = JsonConvert.DeserializeObject<JSONApi>(www.text);
+
+        GameObject.Find("Response").GetComponent<Text>().text = answer.categories[0].detail.landmarks[0].name.ToString().ToUpper();
+    }
+
+
+    public void Analyze()
+    {
+        StartCoroutine(GetInfo());
+    }
+
+
+
+}
---
+++ CognitiveServices/Assets/Scripts/JSONApi.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSONApi : MonoBehaviour
+{
+    public List<Category> categories { get; set; }
+    public string requestId { get; set; }
+    public Metadata metadata { get; set; }
+
+    public class Landmark
+    {
+        public string name { get; set; }
+        public double confidence { get; set; }
+    }
+
+    public class Detail
+    {
+        public List<Landmark> landmarks { get; set; }
+    }
+
+    public class Category
+    {
+        public string name { get; set; }
+        public double score { get; set; }
+        public Detail detail { get; set; }
+    }
+
+    public class Metadata
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public string format { get; set; }
+    }
+
+}
